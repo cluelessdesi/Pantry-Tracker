4342787959ba45ee7b2eaaa33a2f43f04c0457e8
cluelessdesi <sufiyanretreat@gmail.com>
2024-07-31 01:34:18 +0530

    added starting files with a simple box & hello world

---
+++ pantry_tracker/app/page.js
@@ -0,0 +1,15 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+import {Box} from '@mui/material'
+
+const item = ['tomato', 'potato', 'onions', 'apples', 'oranges']
+
+export default function Home() {
+  return ( <Box width="100wh" height="100vh"
+    display={'flex'}
+    justifyContent={'center'}
+    alignItems={'center'}
+    >Hello World</Box>
+  )
+  
+}
