463b1f8778fa4176a16ad9250379286b421b5a84
cluelessdesi <sufiyanretreat@gmail.com>
2024-08-02 15:48:15 +0530

    added some code to the page js file

--- pantry_tracker/app/page.js
+++ pantry_tracker/app/page.js
@@ -1,15 +1,34 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
-import {Box} from '@mui/material'
+import {Box, Stack} from '@mui/material'
 
 const item = ['tomato', 'potato', 'onions', 'apples', 'oranges']
 
 export default function Home() {
-  return ( <Box width="100wh" height="100vh"
+  return (
+    <Box width="100wh" height="100vh"
     display={'flex'}
     justifyContent={'center'}
     alignItems={'center'}
-    >Hello World</Box>
+    >
+
+    <Stack width="800px" height="600px" spacing={2}>
+      {item.map((i) => (
+        <Box
+        key={i}
+        width="100%"
+        height="l0px"
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        bgcolor={'fofof0'}
+        >
+          {i}
+        </Box>
+      ))}
+    </Stack>
+
+    </Box>
   )
   
 }
