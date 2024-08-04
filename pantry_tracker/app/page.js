0ce8e2ffd850c17ce6b4e0afd2d8c01e27774de4
cluelessdesi <sufiyanretreat@gmail.com>
2024-08-04 23:36:51 +0530

    stopped coding for a while

--- pantry_tracker/app/page.js
+++ pantry_tracker/app/page.js
@@ -1,35 +1,64 @@
 // import Image from "next/image";
 // import styles from "./page.module.css";
 "use client"
-import {Box, Stack, Typography} from '@mui/material'
+import {Box, Stack, Typography, Button, Modal, TextField} from '@mui/material'
 import { firestore } from '@/firebase'
-import { collection, getDocs, query } from 'firebase/firestore'
+import { collection, getDocs, query, doc, setDoc, deleteDoc } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
 
 // const item = ['tomato', 'potato', 'onions', 'apples', 'oranges', 'pomegrenate', 'papaya', 'mango']
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadown: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column', 
+  gap: 3,
+}
 
 export default function Home() {
   
   const [pantry, setPantry] = useState([])
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const [itemName, setItemName] = useState('')
+
+  const updatePantry = async() => {
+    const snapshot = query(collection(firestore, 'pantry'))
+    const docs = await getDocs(snapshot)
+    const pantryList = []
+    docs.forEach( (doc) => {
+      pantryList.push(doc.id)
+    })
+    // console.log(pantryList)
+    setPantry(pantryList)
+  }
 
   useEffect(
-    () =>
-    {
-      const updatePantry = async() => {
-        const snapshot = query(collection(firestore, 'pantry'))
-        const docs = await getDocs(snapshot)
-        const pantryList = []
-        docs.forEach( (doc) => {
-          pantryList.push(doc.id)
-        })
-        // console.log(pantryList)
-        setPantry(pantryList)
-      }
-      updatePantry()
-    }, []
+    () => { updatePantry() }, []
   )
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    await setDoc(docRef, {})
+    updatePantry()
+  }
+
+  const delItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    await deleteDoc(docRef).then(() => {
+      updatePantry()
+    })
+  }
 
   return (
     <Box width="100wh" height="100vh"
@@ -37,8 +66,11 @@ export default function Home() {
     justifyContent={'center'}
     alignItems={'center'}
     flexDirection={'column'}
+    gap={2}
     >
     
+    <Button variant="contained">Add Item</Button>
+
     <Box width='800px' height='100px' bgcolor={'#ADD8E6'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
       <Typography variant='h3' color='#333' textAlign={'center'} fontWeight={'bold'}>
         Pantry Items
@@ -49,7 +81,7 @@ export default function Home() {
         <Box
         key={i}
         width="100%"
-        height="300px"
+        minHeight="200px"
         display={'flex'}
         justifyContent={'center'}
         alignItems={'center'}
